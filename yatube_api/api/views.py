7c8720b4491fb120039ada83966da69d0a236824
Aleksandra Shapovalova <shapovalova.ao65@gmail.com>
2024-08-23 15:08:19 +0300

    ver 3

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Comment, Group, Post
 from rest_framework import viewsets
 
+from posts.models import Comment, Group, Post
 from api.permissions import IsAuthorOrReadOnly
 from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
 
